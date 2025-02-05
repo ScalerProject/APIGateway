6f23cd3d5d7c2e5d5c266d34542a8e6bfff65aec
r7sh7 <bolinjkar.rishi@gmail.com>
2025-02-05 17:02:39 +0530

    initialised API Gateway and registered with Eureka server

---
+++ src/main/java/com/scaler/apigatewayjan25/ApiGatewayJan25Application.java
@@ -0,0 +1,13 @@
+package com.scaler.apigatewayjan25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayJan25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayJan25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/apigatewayjan25/ApiGatewayJan25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.apigatewayjan25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayJan25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
